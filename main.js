b99307d03b7e551f98ea8cd7283b80613cd1fc96
olesien <olesien2@gmail.com>
2019-10-21 20:48:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const word = document.querySelector ('#word')
 const Http = new XMLHttpRequest();
 const url='https://random-word-api.herokuapp.com/word?key=CNFJ959F&number=2';
 
+
+//let jsonText = '';
 word.addEventListener('click', function()
 {
 
@@ -11,15 +13,26 @@ Http.open("GET", url);
 Http.send();
 
 Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
+//  console.log(Http.responseText)
+//    jsonText = Http.responseText;
+    words(Http.responseText);
+    
 }
 
 
 });
+
+let word1 = '';
+let word2 = '';
+
+function words(jsonReturn) {
+//    word1 = jsonReturn[0];
+//    word2 = jsonReturn;
+    console.log(jsonReturn);
+}
+
 //Translate this to two variables, word1 and word2
 
-//let word1 = '';
-//let word2 = '';
 
 //Translate the word to swedish
 //let ord1 = '';
