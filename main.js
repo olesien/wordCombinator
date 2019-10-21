bcb0c3e8a7c2bc3c4c306360039111fa7a8d8ac6
olesien <olesien2@gmail.com>
2019-10-21 20:17:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+//DON'T REPEAT YOURSELF YOU PLEB
+
 //https://random-word-api.herokuapp.com/word?key=HKH1FW59&number=2
 //Translate this to two variables, word1 and word2
 
 let word1 = '';
 let word2 = '';
 
-//
+//Translate the word
+let ord1 = '';
+let ord2 = '';
+
+//combine the two
+
+
 
