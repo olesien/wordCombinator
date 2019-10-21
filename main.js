c512a98c15e176eeb9164f83f038585e9e4105fa
weggethegamer <33510329+weggethegamer@users.noreply.github.com>
2019-10-21 20:22:30 +0200

    Yes

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 //DON'T REPEAT YOURSELF YOU PLEB
 
-//https://random-word-api.herokuapp.com/word?key=HKH1FW59&number=2
+const word = document.querySelector ('#word')
+const Http = new XMLHttpRequest();
+const url='https://random-word-api.herokuapp.com/word?key=CNFJ959F&number=2';
+
+word.addEventListener('click', function()
+{
+
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+}
+
+
+});
 //Translate this to two variables, word1 and word2
 
 let word1 = '';
