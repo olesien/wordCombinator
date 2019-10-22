aa7b345998d94739f142767b5c0f142ca914be19
olesien <olesien2@gmail.com>
2019-10-22 09:23:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ word.addEventListener('click', function () {
 
     Http.open("GET", url);
     Http.send();
-
     Http.onreadystatechange = (e) => {
         //  console.log(Http.responseText)
         //    jsonText = Http.responseText;
