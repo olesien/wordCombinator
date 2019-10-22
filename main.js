29d33e9c9b0b5d9ec832a050160a0bac1dd0d4b1
weggethegamer <33510329+weggethegamer@users.noreply.github.com>
2019-10-22 22:29:25 +0200

    Commented

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//DON'T REPEAT YOURSELF YOU PLEB
 const word = document.querySelector('#word')
 const translate = document.querySelector('#Translate')
 const save = document.querySelector('#save')
@@ -7,54 +6,41 @@ const Http = new XMLHttpRequest();
 const api = 'EI85ZMRP';
 const url='https://random-word-api.herokuapp.com/word?key='+ api +'&number=2';
 
-function name()
-{
-var input = document.getElementById("userInput");
-alert(input);
-}
-function getapi() {
-    Http.open("GET", 'https://random-word-api.herokuapp.com/key?');
-    Http.onreadystatechange = (e) => {
-        let apikey = Http.responseText
-        console.log(apikey);
-    }
-};
+    //displays Eng words in separate lines
 function words(lists) {
-
     console.log(lists[1]);
     console.log(lists[0]);
     document.getElementById("word1").innerHTML = "Word 2: " + lists[1];
     document.getElementById("word2").innerHTML = "Word 1: " + lists[0];    
 };
-
-for (let i = 1; i < 20; i++) {
+    //loop it x times
+for (let i = 1; i < 50; i++) {
     setTimeout( function timer(){
+        //gets the english words
         Http.open("GET", url);
     Http.send();
 
     Http.onreadystatechange = (e) => {
         let translatelist = JSON.parse(Http.responseText);
-
+        //traslates words
         words(translatelist);
         Http.open("GET", "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20191021T192454Z.c43817007eac43cd.9ac53323ffbcbf6aeb330f858abd1dd2b0f84b8c&text=" + translatelist + "&lang=en-sv");
         Http.send();
+        //parses the words
         Http.onreadystatechange = (e) => {
             var json = Http.responseText;
             obj = JSON.parse(json)
-
-            // log eng words.
+            //log Translated words to console
             console.log("Translated:" + obj.text)
-            //document.getElementById("Translated").innerHTML = "Translated: " + obj.text;
             
- 
         }
-        // inner HTML REAULT 
-        var node = document.createElement("LI");
+            // Creates a li and puts resault in it.
+            var node = document.createElement("LI");
             var textnode = document.createTextNode(obj.text);
             node.appendChild(textnode);
             document.getElementById("myList").appendChild(node);
             
     }
-    }, i*700 );
+    }, i*500 );
 }
 
