3e6122164e8f6b14b63401c9157eb67f1d8daeef
olesien <olesien2@gmail.com>
2019-10-22 11:33:19 +0200

    idk what i did tbh

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const word = document.querySelector('#word')
 const translate = document.querySelector('#Translate')
 const save = document.querySelector('#save')
 const Http = new XMLHttpRequest();
-const api = 'WYTL0QZM'
+const api = 'R1RVP6DI'
 const url = 'https://random-word-api.herokuapp.com/word?key=' + api + '&number=2';
 
 word.addEventListener('click', function () {
