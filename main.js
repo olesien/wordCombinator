83f1b8811ac00ed1b2e6ab07ec55509552ae228a
weggethegamer <33510329+weggethegamer@users.noreply.github.com>
2019-10-22 12:48:57 +0200

    Update fullu working
    Exept download

--- main.js
+++ main.js
@@ -3,16 +3,35 @@
 const word = document.querySelector('#word')
 const translate = document.querySelector('#Translate')
 const save = document.querySelector('#save')
+const myList = document.querySelector('#myList')
 const Http = new XMLHttpRequest();
+const api = 'EI85ZMRP';
 const url='https://random-word-api.herokuapp.com/word?key='+ api +'&number=2';
-const api = 'UMVJHFQY'
+
+
+function getapi() {
+    Http.open("GET", 'https://random-word-api.herokuapp.com/key?');
+    Http.onreadystatechange = (e) => {
+        let apikey = Http.responseText
+        console.log(apikey);
+    }
+};
+function words(lists) {
+
+    console.log(lists[1]);
+    console.log(lists[0]);
+    document.getElementById("word1").innerHTML = "Word 2: " + lists[1];
+    document.getElementById("word2").innerHTML = "Word 1: " + lists[0];    
+};
 
 word.addEventListener('click', function () {
 
     Http.open("GET", url);
     Http.send();
+
     Http.onreadystatechange = (e) => {
         let translatelist = JSON.parse(Http.responseText);
+
         words(translatelist);
         Http.open("GET", "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20191021T192454Z.c43817007eac43cd.9ac53323ffbcbf6aeb330f858abd1dd2b0f84b8c&text=" + translatelist + "&lang=en-sv");
         Http.send();
@@ -20,26 +39,25 @@ word.addEventListener('click', function () {
             var json = Http.responseText;
             obj = JSON.parse(json)
 
+            // log eng words.
             console.log("Translated:" + obj.text)
             document.getElementById("Translated").innerHTML = "Translated: " + obj.text;
+            
+            
+            
         }
+        // inner HTML REAULT 
+        var node = document.createElement("LI");
+            var textnode = document.createTextNode(obj.text);
+            node.appendChild(textnode);
+            document.getElementById("myList").appendChild(node);
     }
 
 
 });
 
-function words(lists) {
-
-    console.log(lists[1]);
-    console.log(lists[0]);
-    document.getElementById("word1").innerHTML = "Word 2: " + lists[1];
-    document.getElementById("word2").innerHTML = "Word 1: " + lists[0];
-};
-
-// save to file **** NOT WORKING ****
-
 save.addEventListener('click', function () {
-    var textToSave = Http.responseText;
+    var textToSave = 'test 123';
 
     var hiddenElement = document.createElement('a');
 
